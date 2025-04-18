922768d61e0071e16d13d0a6bcf555e214c412f8
Mohit Nagaraj <mohitnagaraj20@gmail.com>
2025-04-18 13:07:01 +0000

    deployment to k3 part1

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM mcr.microsoft.com/devcontainers/go:1.23
-
-RUN curl -sfL https://get.k3s.io | sh -s - --disable traefik --disable servicelb
-
-ENV KUBECONFIG=/etc/rancher/k3s/k3s.yaml
---
+++ hack/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Step 1: Navigate to the app directory
+cd ../app || exit
+
+# Step 2: Build the Docker image
+echo "Building Docker image 'go-k3-app'..."
+docker build -t go-k3-app .
+
+# Step 3: Return to the root directory
+cd ..
+
+# Done
+echo "✅ Docker image 'go-k3-app' built successfully."
